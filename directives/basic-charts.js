c48088a1c5777abac6f39ca635518ff165aeb86d
dzen <dzentoo@gmail.com>
2013-02-09 12:05:24 +0100

    Add Scatterplot

--- directives/basic-charts.js
+++ directives/basic-charts.js
@@ -913,8 +913,115 @@ d3js.directive('groupedbarchart', function() {
   };
 });
 
-d3js.directive('Scatterplot', function() {
+d3js.directive('scatterplot', function() {
+  // define constants and helpers used for the directive
+  // ...
+  return {
+    restrict: 'E', // the directive can be invoked only by using  tag in the template
+    scope: { // attributes bound to the scope of the directive
+      val: '='
+    },
+    link: function (scope, element, attrs) {
+        var data_url = attrs['tsv'];
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
+        var x = d3.scale.linear()
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var color = d3.scale.category10();
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.tsv(data_url, function(error, data) {
+          data.forEach(function(d) {
+            d.sepalLength = +d.sepalLength;
+            d.sepalWidth = +d.sepalWidth;
+          });
+
+          x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
+          y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("x", width)
+              .attr("y", -6)
+              .style("text-anchor", "end")
+              .text("Sepal Width (cm)");
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Sepal Length (cm)")
+
+          svg.selectAll(".dot")
+              .data(data)
+            .enter().append("circle")
+              .attr("class", "dot")
+              .attr("r", 3.5)
+              .attr("cx", function(d) { return x(d.sepalWidth); })
+              .attr("cy", function(d) { return y(d.sepalLength); })
+              .style("fill", function(d) { return color(d.species); });
+
+          var legend = svg.selectAll(".legend")
+              .data(color.domain())
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+          legend.append("rect")
+              .attr("x", width - 18)
+              .attr("width", 18)
+              .attr("height", 18)
+              .style("fill", color);
+
+          legend.append("text")
+              .attr("x", width - 24)
+              .attr("y", 9)
+              .attr("dy", ".35em")
+              .style("text-anchor", "end")
+              .text(function(d) { return d; });
+
+        });
+
+
+      // initialization, done once per my-directive tag in template. If my-directive is within an
+      // ng-repeat-ed template then it will be called every time ngRepeat creates a new copy of the template.
+      
+      // ...
+      
+      // whenever the bound 'exp' expression changes, execute this 
+      scope.$watch('exp', function (newVal, oldVal) {
+        // ...
+      });
+    }
+  };
 });
 
 d3js.directive('donutchart', function() {
