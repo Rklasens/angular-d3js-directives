7917c9fecf68766bb07cc6e9c893c7f7e20b0b7e
dzen <dzentoo@gmail.com>
2013-02-09 11:58:14 +0100

    Add Grouped Bar Chart & Normalized Stacked Bar Chart

--- directives/basic-charts.js
+++ directives/basic-charts.js
@@ -696,12 +696,221 @@ d3js.directive('stackedbarchart', function() {
     }
   };
 });
-d3js.directive('NormalizedStackedBarChart', function() {
+d3js.directive('normalizedstackedbarchart', function() {
+  // define constants and helpers used for the directive
+  // ...
+  return {
+    restrict: 'E', // the directive can be invoked only by using  tag in the template
+    scope: { // attributes bound to the scope of the directive
+      val: '='
+    },
+    link: function (scope, element, attrs) {
+        var data_url = attrs['csv'];
+        var margin = {top: 20, right: 100, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .rangeRound([height, 0]);
+
+        var color = d3.scale.ordinal()
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(d3.format(".0%"));
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.csv(data_url, function(error, data) {
+          color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+
+          data.forEach(function(d) {
+            var y0 = 0;
+            d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+            d.ages.forEach(function(d) { d.y0 /= y0; d.y1 /= y0; });
+          });
+
+          data.sort(function(a, b) { return b.ages[0].y1 - a.ages[0].y1; });
+
+          x.domain(data.map(function(d) { return d.State; }));
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis);
+
+          var state = svg.selectAll(".state")
+              .data(data)
+            .enter().append("g")
+              .attr("class", "state")
+              .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+
+          state.selectAll("rect")
+              .data(function(d) { return d.ages; })
+            .enter().append("rect")
+              .attr("width", x.rangeBand())
+              .attr("y", function(d) { return y(d.y1); })
+              .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+              .style("fill", function(d) { return color(d.name); });
+
+          var legend = svg.select(".state:last-child").selectAll(".legend")
+              .data(function(d) { return d.ages; })
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d) { return "translate(" + x.rangeBand() / 2 + "," + y((d.y0 + d.y1) / 2) + ")"; });
+
+          legend.append("line")
+              .attr("x2", 10);
+
+          legend.append("text")
+              .attr("x", 13)
+              .attr("dy", ".35em")
+              .text(function(d) { return d.name; });
+        });
+
+      // initialization, done once per my-directive tag in template. If my-directive is within an
+      // ng-repeat-ed template then it will be called every time ngRepeat creates a new copy of the template.
+      
+      // ...
+      
+      // whenever the bound 'exp' expression changes, execute this 
+      scope.$watch('exp', function (newVal, oldVal) {
+        // ...
+      });
+    }
+  };
 });
 
-d3js.directive('GroupedBarChart', function() {
+d3js.directive('groupedbarchart', function() {
+  // define constants and helpers used for the directive
+  // ...
+  return {
+    restrict: 'E', // the directive can be invoked only by using  tag in the template
+    scope: { // attributes bound to the scope of the directive
+      val: '='
+    },
+    link: function (scope, element, attrs) {
+        var data_url = attrs['csv'];
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
+        var x0 = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var x1 = d3.scale.ordinal();
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var color = d3.scale.ordinal()
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x0)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(d3.format(".2s"));
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.csv(data_url, function(error, data) {
+          var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+
+          data.forEach(function(d) {
+            d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+          });
+
+          x0.domain(data.map(function(d) { return d.State; }));
+          x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+          y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Population");
+
+          var state = svg.selectAll(".state")
+              .data(data)
+            .enter().append("g")
+              .attr("class", "g")
+              .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+
+          state.selectAll("rect")
+              .data(function(d) { return d.ages; })
+            .enter().append("rect")
+              .attr("width", x1.rangeBand())
+              .attr("x", function(d) { return x1(d.name); })
+              .attr("y", function(d) { return y(d.value); })
+              .attr("height", function(d) { return height - y(d.value); })
+              .style("fill", function(d) { return color(d.name); });
+
+          var legend = svg.selectAll(".legend")
+              .data(ageNames.slice().reverse())
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+          legend.append("rect")
+              .attr("x", width - 18)
+              .attr("width", 18)
+              .attr("height", 18)
+              .style("fill", color);
+
+          legend.append("text")
+              .attr("x", width - 24)
+              .attr("y", 9)
+              .attr("dy", ".35em")
+              .style("text-anchor", "end")
+              .text(function(d) { return d; });
+        });
+
+
+      // initialization, done once per my-directive tag in template. If my-directive is within an
+      // ng-repeat-ed template then it will be called every time ngRepeat creates a new copy of the template.
+      
+      // ...
+      
+      // whenever the bound 'exp' expression changes, execute this 
+      scope.$watch('exp', function (newVal, oldVal) {
+        // ...
+      });
+    }
+  };
 });
 
 d3js.directive('Scatterplot', function() {
